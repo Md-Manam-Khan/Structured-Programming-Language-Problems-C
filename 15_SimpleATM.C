5167128cc1fbe93065119492ed9d2589847182b3
Md. Manam Khan <manamkhan921@gmail.com>
2026-09-05 02:54:19 +0600

    New topics learned and problems solved

--- 15_SimpleATM.C
+++ 15_SimpleATM.C
@@ -1,28 +1,25 @@
 #include <stdio.h>
-
 int main()
 {
     int choice;
     float balance = 10000, amount;
-
     printf("1. Check Balance\n");
     printf("2. Deposit\n");
     printf("3. Withdraw\n");
     printf("4. Exit\n");
-
     printf("Choose an option: ");
     scanf("%d", &choice);
-
     switch(choice)
     {
         case 1:
+        {
             printf("Current balance: %.2f", balance);
             break;
-
+        }
         case 2:
+        {
             printf("Input deposit amount: ");
             scanf("%f", &amount);
-
             if(amount > 0)
             {
                 balance = balance + amount;
@@ -33,13 +30,12 @@ int main()
             {
                 printf("Invalid amount");
             }
-
             break;
-
+        }
         case 3:
+        {
             printf("Input withdrawal amount: ");
             scanf("%f", &amount);
-
             if(amount <= 0)
             {
                 printf("Invalid amount");
@@ -54,15 +50,17 @@ int main()
                 printf("Withdrawal successful\n");
                 printf("Current balance: %.2f", balance);
             }
-
             break;
-
+        }  
         case 4:
+        {
             printf("Thank you for using the ATM");
             break;
-
+        }
         default:
+        {
             printf("Invalid choice");
+        }    
     }
     return 0;
 }
